a104490d783df0b0d04f4c9c8af16455348311a0
XB Developers <sahugauravprasad@yahoo.com>
2021-04-25 22:52:33 +0530

    Data Fix

--- custom-spring-security/src/main/resources/data.sql
+++ custom-spring-security/src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO users (username, password, enabled) values ('alex', '1234', true);
 INSERT INTO users (username, password, enabled) values ('parul', 'zxcv', true);
 INSERT INTO users (username, password, enabled) values ('gaurav', 'qwerty', true);
 
-INSERT INTO authorities (username, authority) values ('ekta', 'ROLE_USER');
+INSERT INTO authorities (username, authority) values ('alex', 'ROLE_USER');
 INSERT INTO authorities (username, authority) values ('parul', 'ROLE_ADMIN');
 INSERT INTO authorities (username, authority) values ('gaurav', 'ROLE_XYZ');
